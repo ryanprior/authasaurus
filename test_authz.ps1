40b1850ffb6ebe190b28f1458e0e509ddfaa7821
David Ullmann <davidl.ullmann@gmail.com>
2020-05-20 19:19:57 -0400

    added test script

---
+++ test_authz.ps1
@@ -0,0 +1,5 @@
+﻿$db_id = [guid]::NewGuid().ToString()
+$env:AUTHZ_DB_FILE = 'DB_Authorizaion ' + $db_id
+$server = Start-Process py -ArgumentList '-m', 'authz.test.server'
+py -m authz.test.tests $args
+Stop-Process $server
