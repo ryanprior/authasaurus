d2eac4129483736f6b1de2bf0d9e7e33a4e3c923
David Ullmann <davidl.ullmann@gmail.com>
2020-05-26 17:44:58 -0400

    put test databases in test_data folder

--- test_authz.ps1
+++ test_authz.ps1
@@ -1,5 +1,12 @@
 ﻿$db_id = [guid]::NewGuid().ToString()
-$env:AUTHZ_DB_FILE = 'DB_Authorizaion ' + $db_id
-$server = Start-Process py -ArgumentList '-m', 'authz.test.server'
+$env:AUTHZ_DB_FILE = 'test_data\\Authorization ' + $db_id
+$server = Start-Process py -ArgumentList '-m', 'authz.test.server' -PassThru 
+
+Write-Output $server.Id
+
+#Start-Sleep -Seconds 2
+
 py -m authz.test.tests $args
-Stop-Process $server
+
+Stop-Process -Id $server.Id
+
