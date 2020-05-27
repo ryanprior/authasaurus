c647194cf88f4e488739263daa1f83701612690c
David Ullmann <davidl.ullmann@gmail.com>
2020-05-27 18:50:45 -0400

    create api_key when user logs in

--- authz/service.py
+++ authz/service.py
@@ -1,16 +1,18 @@
 from flask import request, redirect
 from .decorators import auth_login, auth_required
 from . import settings
-from .db import rotate_api_key
+from .db import rotate_api_key, create_api_key
+from datetime import datetime, timedelta
 
 @auth_login
 def login(authz):
     url = request.args.get("redirect") or request.form.get("redirect", "/")
     response = redirect(url)
     user, _ = authz
+    api_key = create_api_key(user.user_id, 2, datetime.now() + timedelta(days=10))
     response.set_cookie(
         "api-key",
-        value=user.api_key,
+        value=api_key.key,
         max_age=60 * 60 * 24 * 10,  # 10 days
         secure=True,
         httponly=(not settings.allow_javascript_to_read_api_key),
