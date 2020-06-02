ab6c53b21b1f56b99b1e922f14bed41a384c5b75
Ryan Prior <ryanprior@gmail.com>
2020-06-02 01:36:55 -0500

    Updates formatting of tests.py

--- authz/test/tests.py
+++ authz/test/tests.py
@@ -10,9 +10,7 @@ import unittest
 endpoint = "http://localhost:5000"
 
 
-
 class AuthzTests(unittest.TestCase):
-
     @classmethod
     def setUpClass(self):
         super(AuthzTests, self).setUpClass()
@@ -27,7 +25,7 @@ class AuthzTests(unittest.TestCase):
     def test_call_incorrect_authentication(self):
         headers = {"Authorization": "token: asdfsadfsadfadsfadsfdsafdsaf"}
         resp = requests.get(endpoint, headers=headers)
-        self.assertEqual(resp.status_code,  http.client.UNAUTHORIZED)
+        self.assertEqual(resp.status_code, http.client.UNAUTHORIZED)
 
     def test_call_correct_authentication(self):
         api_key = create_api_key(self.user.user_id)
@@ -59,7 +57,6 @@ class AuthzTests(unittest.TestCase):
         resp = requests.get(url, cookies=cookies)
         self.assertEqual(resp.status_code, http.client.OK)
 
-
     def test_call_with_invalid_username_in_path(self):
         api_key = create_api_key(self.unauthorized_user.user_id)
 
@@ -72,7 +69,6 @@ class AuthzTests(unittest.TestCase):
         resp = requests.get(endpoint + "/admin/history", headers=headers)
         self.assertEqual(resp.status_code, http.client.UNAUTHORIZED)
 
-
     def test_call_with_valid_username_in_path(self):
         api_key = create_api_key(self.admin.user_id)
 
@@ -84,23 +80,24 @@ class AuthzTests(unittest.TestCase):
         resp = requests.get(endpoint + "/admin/history", headers=headers)
         self.assertEqual(resp.status_code, http.client.OK)
 
-
     def test_login(self):
         referrer = "/login"
         redirect = "/"
         url = endpoint + f"/login?referrer={referrer}&redirect={redirect}"
 
-        #first test that we can't login with an API key in headers
+        # first test that we can't login with an API key in headers
         api_key = create_api_key(self.admin.user_id)
         headers = {"Authorization": f"Token {api_key.key}"}
         resp = requests.post(url, headers=headers, allow_redirects=False)
         self.assertEqual(resp.status_code, http.client.FOUND)
         self.assertEqual(resp.headers.get("Location"), endpoint + referrer)
         self.assertEqual(resp.cookies.get("api-key"), None)
-        
-        #test that we can login with basic HTTP auth
+
+        # test that we can login with basic HTTP auth
         resp = requests.post(
-            url, auth=HTTPBasicAuth(self.admin.name, self.admin_password), allow_redirects=False
+            url,
+            auth=HTTPBasicAuth(self.admin.name, self.admin_password),
+            allow_redirects=False,
         )
         self.assertEqual(resp.status_code, http.client.FOUND)
         self.assertEqual(resp.headers.get("Location"), endpoint + redirect)
