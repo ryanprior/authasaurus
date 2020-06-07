6e8290cb5e12c3fa285f2e99b0c415b36295b1f7
Ryan Prior <ryanprior@gmail.com>
2020-06-06 22:04:08 -0500

    Adds doc strings to test/server.py

--- authz/test/server.py
+++ authz/test/server.py
@@ -1,3 +1,5 @@
+"""Simple Flask web service to test authz flows."""
+
 from logging import basicConfig, NOTSET
 from os import environ as env
 from flask import Flask
@@ -10,24 +12,28 @@ app = Flask(__name__)
 @app.route("/")
 @auth_required
 def index():
+    """Send ok if authorized as any user."""
     return "ok\n", 200
 
 
 @app.route("/protected")
 @auth_required(users=["admin"])
 def protected():
+    """Greet admin; reject others."""
     return "hello admin\n", 200
 
 
 @app.route("/<username>/account")
 @auth_user
 def account_settings(username):
+    """Show a message only to the authorized user whose name matches the USERNAME param."""
     return "set your password", 200
 
 
 @app.route("/<user>/history")
 @auth_user(arg="user")
 def user_history(user):
+    """Show a message only to the authz user whose name matches the USER param."""
     return "set your password", 200
 
 
