01914d2b3ebb9fb9ee0365686eea16086b08f41e
Ryan Prior <ryanprior@hey.com>
2021-03-04 00:13:25 -0600

    Adds user registration, login and logout routes to standalone server

--- authz/standalone/server.py
+++ authz/standalone/server.py
@@ -1,29 +1,32 @@
 """Standalone Authasaurus reverse proxy."""
 
-from http.server import HTTPServer, BaseHTTPRequestHandler
+from flask import Flask, request
+from ..decorators import auth_required, auth_user
+from .. import service
+from ..db import create_user, get_user
+from json import dumps as encode
 
+app = Flask(__name__)
 
-class ProxyHTTPRequestHandler(BaseHTTPRequestHandler):
-    protocol_version = 'HTTP/1.0'
+def empty(field):
+    return (not field) or len(field.strip()) == 0
 
-    def do_POST(self, body=True):
-        try:
-            req_header = self.headers
-            body = self.rfile.read()
-            self.rfile.close()
-            print(f"Got body: {body}")
-            self.send_response(200)
-            for key, value in req_header.items():
-                self.send_header(key, value)
-            # self.send_resp_headers(req_header, 11)
-            self.end_headers()
-            self.wfile.write(bytes(f"Request: {self.requestline}\n", 'utf-8'))
-            # self.wfile.write(body)
-        finally:
-            self.wfile.flush()
+@app.route("/register", methods=["POST"])
+def register():
+    username = request.form.get("username")
+    if empty(username):
+        return "Request body must include a non empty username.\n", 400
+    if get_user(username = username):
+        return "Provided username is already registered.\n", 400
+    user, password = create_user(username, login = True)
+    return encode({
+        "username": user.name,
+        "password": password
+    }), 200
+
+login = app.route("/login", methods=["POST"])(service.login)
+logout = app.route("/logout", methods=["POST"])(service.logout)
 
 if __name__ == '__main__':
-    server_address = ('127.0.0.1', 8081)
-    httpd = HTTPServer(server_address, ProxyHTTPRequestHandler)
-    print('http server is running')
-    httpd.serve_forever()
+    address, port = ('127.0.0.1', 8081)
+    app.run(debug=True, host=address, port=port)
